6e0d2ff7567b6ba73402d51bdab8e8b6455cbba2
emnixa <102804220+emnixa@users.noreply.github.com>
2022-04-12 14:13:38 +0200

    Change style
    Changes the fonts and background colors as well as some sticky variables to improve visuals

--- Data Collection/data_collection_gui.py
+++ Data Collection/data_collection_gui.py
@@ -5,7 +5,7 @@ class DataCollection:
     def __init__(self, root):
         self.frame_header = ttk.Frame(root)
         self.frame_header.pack()
-        ttk.Label(self.frame_header, text='Rock Paper Scissors Data Collection').pack()
+        ttk.Label(self.frame_header, text='Rock Paper Scissors Data Collection',style='Header.TLabel').pack()
         ttk.Label(self.frame_header, text='Set your meta data, record, play, save').pack()
 
         self.frame_meta = ttk.Frame(root)
@@ -16,30 +16,45 @@ class DataCollection:
         ttk.Radiobutton(self.frame_meta, text='Paper', variable=self.result, value='Paper').grid(row=1,column=0, sticky='w')
         ttk.Radiobutton(self.frame_meta, text='Scissors', variable=self.result, value='Scissors').grid(row=2,column=0, sticky='w')
         
-        ttk.Label(self.frame_meta,text='Name:').grid(row=0,column=1,sticky='w')
-        ttk.Label(self.frame_meta,text='Angle:').grid(row=1,column=1,sticky='w')
-        ttk.Label(self.frame_meta,text='Counts:').grid(row=2,column=1,sticky='w')
+        ttk.Label(self.frame_meta,text='Name:').grid(row=0,column=1,sticky='e',padx=5)
+        ttk.Label(self.frame_meta,text='Angle:').grid(row=1,column=1,sticky='e',padx=5)
+        ttk.Label(self.frame_meta,text='Counts:').grid(row=2,column=1,sticky='e',padx=5)
 
-        self.entry_name = ttk.Entry(self.frame_meta).grid(row=0,column=2,sticky='w')
+        self.entry_name = ttk.Entry(self.frame_meta)
+        self.entry_name.grid(row=0,column=2,sticky='w')
         self.angle_of_hand = StringVar()
         self.angle = ttk.Combobox(self.frame_meta, textvariable=self.angle_of_hand,
-                                  values=('inside','in front','outside')).grid(row=1,column=2,sticky='w')
-        self.count = ttk.Label(self.frame_meta, text='There are no data points with this meta combo').grid(row=2,column=2,sticky='w')
+                                  values=('inside','in front','outside'))
+        self.angle.grid(row=1,column=2,sticky='w')
+        self.count = ttk.Label(self.frame_meta, text='There are no data points with this meta combo')
+        self.count.config(wraplength=180)
+        self.count.grid(row=2,column=2,sticky='sw')
         
         self.frame_record = ttk.Frame(root)
         self.frame_record.pack()
 
-        self.logo = PhotoImage(file=r'C:\Users\emnixa\Documents\RockPaperScissors\Data Collection\peace.png').subsample(10,10)
-        self.video = ttk.Label(self.frame_record,image=self.logo).grid(row=0,column=0,sticky='w')
+        self.logo = PhotoImage(file=r'C:\Users\emnixa\Documents\RockPaperScissors\Data Collection\peace.png')
+        self.video = ttk.Label(self.frame_record,image=self.logo).grid(row=0,column=0,sticky='w',padx=10)
 
-        ttk.Button(self.frame_record,text='Play',command=self.play).grid(row=0,column=1,sticky='se')
+        ttk.Button(self.frame_record,text='Play',command=self.play).grid(row=0,column=1,sticky='e',padx=10)
     
     def play(self):
         print('Play!')
 
 def main():
     root = Tk()
-    data_collection = DataCollection(root)
+    root.title('Data Collection')
+    root.resizeable=(False,False)
+    root.configure(background='#e1d8b9')
+
+    style = ttk.Style()
+    style.configure('TFrame', background='#e1d8b9', font=('Arial',11))
+    style.configure('TLabel', background='#e1d8b9', font=('Arial',11))
+    style.configure('Header.TLabel', font=('Arial',18,'bold'))
+    style.configure('TButton', background='#e1d8b9', font=('Arial',11))
+    style.configure('TCombobox', background='#e1d8b9', font=('Arial',11))
+    style.configure('TRadiobutton', background='#e1d8b9', font=('Arial',11))
+    DataCollection(root)
     root.mainloop()
 
 if __name__ == '__main__':
