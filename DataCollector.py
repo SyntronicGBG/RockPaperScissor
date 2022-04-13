910cd2a2f5bd257fbb79e021034e09c25b9edebb
Agaton Fransson <agfrxa@syntronic.com>
2022-04-13 15:00:05 +0200

    Removes cropping to only enable scale conserving resizing, also removes some hardcoding

--- DataCollector.py
+++ DataCollector.py
@@ -1,12 +1,13 @@
 import cv2
-def record_video(fps, seconds, crop_width=640, crop_height=480, scale_factor=1, ):
+def record_video(fps, seconds, scale_factor=1):
     number_of_frames = int(fps * seconds)
     
     # write something that makes the video go to server
     cap = cv2.VideoCapture(0)
     
-    width= int(crop_width * scale_factor) # max 640
-    height= int(crop_height * scale_factor) # max 480
+
+    width= int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)) # 640
+    height= int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)) # 480
     writer= cv2.VideoWriter('basicvideo.mp4', cv2.VideoWriter_fourcc(*'DIVX'), fps, (width, height))
     
     counter = 0
@@ -14,8 +15,7 @@ def record_video(fps, seconds, crop_width=640, crop_height=480, scale_factor=1, 
         counter += 1
         
         ret,frame = cap.read()
-        # enables cropping and resizing, commented out until further notice
-        frame = cv2.resize(frame[:,80:560], (0,0), fx=scale_factor, fy=scale_factor)
+        frame = cv2.resize(frame, (0,0), fx=scale_factor, fy=scale_factor)
         writer.write(frame)
     
         cv2.imshow('frame', frame)
