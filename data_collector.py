5d4be33d77831d469c7782608cd9bba72223c304
emnixa <102804220+emnixa@users.noreply.github.com>
2022-04-25 10:45:32 +0200

    Adds countdown before recording

--- data_collector.py
+++ data_collector.py
@@ -6,12 +6,14 @@
 # eg. 0.5 halves the width and height of the video.
 
 
+from math import ceil
 import cv2
 import pandas as pd
 from datetime import datetime
 import os
 from ssh_connection import SSHConnection
 from sql_connection import SQLConnection
+import time
 
 class DataCollector:
     def __init__(self):
@@ -53,21 +55,37 @@ class DataCollector:
         meta_data['pixel_height'] = height
         meta_data['record_time'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
+        start_time = time.time()
+        while time.time()-start_time<3:
+            self.handle_frame(str(ceil(3-(time.time()-start_time))), (0,255,255), scale_factor)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        
         total_of_frames = meta_data['number_frames']
         counter_of_frames = 0
         while counter_of_frames < total_of_frames:
             counter_of_frames += 1
 
-            ret, frame = self.cap.read()
-            frame = cv2.resize(frame, (0,0), fx=scale_factor, fy=scale_factor)
-            writer.write(frame)
-
-            cv2.imshow('frame', frame)
+            self.handle_frame('Recording', (0,0,255), scale_factor, writer)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
+        self.handle_frame('Done', (0,255,0), scale_factor)
         writer.release()
 
+    def handle_frame(self, text, color, scale_factor, writer=None):
+        _, frame = self.cap.read()
+        frame = cv2.resize(frame, (0,0), fx=scale_factor, fy=scale_factor)
+        if writer:
+            writer.write(frame)
+        cv2.putText(frame,
+                    text,
+                    (50,50),
+                    cv2.FONT_HERSHEY_COMPLEX, 1,
+                    color, 2, cv2.LINE_4)
+        cv2.imshow('frame', frame)
+    
     def transfer_video(self, meta_data):
         """Transfers the video to the sql-database.
 
